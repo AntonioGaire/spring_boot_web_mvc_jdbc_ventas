2244269546428959a065871e3f86c381d5928aac
Antonio Gaire <agarjim0709@g.educaand.es>
2022-12-13 11:11:33 +0100

    commit comercialdao;

--- src/main/java/org/iesvdm/SpringBootWebMvcJdbcVentasApplication.java
+++ src/main/java/org/iesvdm/SpringBootWebMvcJdbcVentasApplication.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import org.iesvdm.dao.ClienteDAO;
 import org.iesvdm.modelo.Cliente;
+import org.iesvdm.dao.ComercialDAO;
+import org.iesvdm.modelo.Comercial;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,6 +19,9 @@ public class SpringBootWebMvcJdbcVentasApplication implements CommandLineRunner{
 
 	@Autowired
 	private ClienteDAO clienteDAO;
+	
+	@Autowired
+	private ComercialDAO comercialDAO;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebMvcJdbcVentasApplication.class, args);
@@ -70,6 +75,53 @@ public class SpringBootWebMvcJdbcVentasApplication implements CommandLineRunner{
 		
 		log.info("************************************");
 		log.info("*FIN: Prueba de arranque ClienteDAO*");
+		log.info("************************************");
+		
+		log.info("*********************************");
+		log.info("*Prueba de arranque ComercialDAO*");
+		log.info("*********************************");
+		
+		comercialDAO.getAll().forEach(c -> log.info("Comercial: {}", c));
+		
+		id = 1;
+		Optional<Comercial> comercial = comercialDAO.find(id);
+		
+		if (comercial.isPresent()) {
+			log.info("Comercial {}: {}", id, comercial.get());
+			
+			String nombreOld = comercial.get().getNombre();
+			
+			comercial.get().setNombre("Juan Modi");
+			
+			comercialDAO.update(comercial.get());
+			
+			comercial = comercialDAO.find(id);
+			
+			log.info("Comercial {}: {}", id, comercial.get());
+			
+			//Volvemos a cargar el nombre antiguo..
+			comercial.get().setNombre(nombreOld);
+			comercialDAO.update(comercial.get());
+			
+		}
+		
+		// Como es un cliente nuevo a persistir, id a 0
+		Comercial comercialNew = new Comercial(0, "Juan Modi", "modifi", null, (float)(0.225));
+		
+		//create actualiza el id
+		comercialDAO.create(comercialNew);
+		
+		log.info("Comercial nuevo con id = {}", comercialNew.getId());
+		
+		comercialDAO.getAll().forEach(c -> log.info("Comercial: {}", c));
+		
+		//borrando por el id obtenido de create
+		comercialDAO.delete(comercialNew.getId());
+		
+		comercialDAO.getAll().forEach(c -> log.info("Comercial: {}", c));
+		
+		log.info("************************************");
+		log.info("*FIN: Prueba de arranque ComercialDAO*");
 		log.info("************************************");
 		
 	}
---
+++ src/main/java/org/iesvdm/modelo/Comercial.java
@@ -0,0 +1,19 @@
+package org.iesvdm.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+//La anotación @Data de lombok proporcionará el código de: 
+//getters/setters, toString, equals y hashCode
+//propio de los objetos POJOS o tipo Beans
+@Data
+//Para generar un constructor con lombok con todos los args
+@AllArgsConstructor
+@SuppressWarnings("unused")
+
+public class Comercial {
+	private int id;
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private float comision;
+}
